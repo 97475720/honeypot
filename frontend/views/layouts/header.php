eca8084ab91f4eb4944e565a3ad1efa386b8b3cb
BaiJunAn <97475720@qq.com>
2018-04-16 12:52:49 +0800

    new project

---
+++ frontend/views/layouts/header.php
@@ -0,0 +1,19 @@
+<?php
+use yii\helpers\Url;
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="<?= Url::to('@web/css/honeypot.css') ?>" rel="stylesheet">
+</head>
+<body>
+
+<?= $content; ?>
+
+</body>
+</html>
