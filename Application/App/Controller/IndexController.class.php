d3e3c3a23ee0afe8a29c1a5d6d4078a3eb1e70dd
BaiJunAn <97475720@qq.com>
2018-06-26 12:32:58 +0800

    OK

--- Application/App/Controller/IndexController.class.php
+++ Application/App/Controller/IndexController.class.php
@@ -89,34 +89,60 @@ class IndexController extends Controller {
      */
     public function collectCases()
     {
+        $type = I('post.type');
         $cases_id = I('post.cases_id');
         $user_id = I('post.user_id');
-        $cases = $this->casesModel
-            ->where(['id'=>$cases_id])
-            ->find();
-        if($user_id == $cases['user_id']){
-            json(110,"不能收藏自己的作品",(object)array());
-        }
         $where['user_id'] = $user_id;
         $where['cases_id'] = $cases_id;
-        $is_collect = $this->collectModel
-            ->where($where)
-            ->find();
-        if($is_collect){
-            json(110,'请勿重复收藏',(object)array());
+        switch ($type){
+            case 1:
+                $cases = $this->casesModel
+                    ->where(['id'=>$cases_id])
+                    ->find();
+                if($user_id == $cases['user_id']){
+                    json(110,"不能收藏自己的作品",(object)array());
+                }
+                $is_collect = $this->collectModel
+                    ->where($where)
+                    ->find();
+                if($is_collect){
+                    json(110,'请勿重复收藏',(object)array());
+                }
+                $this->collectModel->startTrans();
+                if($this->collectModel->create('','collectCases') === false){
+                    json(110,'收藏失败',(object)array());
+                }
+                if($this->collectModel->add() === false){
+                    json(110,'收藏失败',(object)array());
+                }
+                if($this->casesModel->where(['id'=>$cases_id])->setInc('collect_count',1) === false){
+                    json(110,'收藏失败',(object)array());
+                }
+                $this->collectModel->commit();
+                json('','',(object)array());
+
+            case 2:
+                $is_collect = $this->collectModel
+                    ->where($where)
+                    ->find();
+                if(!$is_collect){
+                    json(110,'请先收藏该作品',(object)array());
+                }
+                $this->collectModel->startTrans();
+                if($this->collectModel->where($where)->delete() === false){
+                    $this->collectModel->rollback();
+                    json(110,'取消收藏失败',(object)array());
+                }
+                if($this->casesModel->where(['id'=>$cases_id])->setDec('collect_count',1) === false){
+                    $this->collectModel->rollback();
+                    json(110,'取消收藏失败',(object)array());
+                }
+                $this->collectModel->commit();
+                json('','',(object)array());
+            default:
+                json(110,'操作失败,缺少必须参数',(object)array());
         }
-        $this->collectModel->startTrans();
-        if($this->collectModel->create('','collectCases') === false){
-            json(110,'收藏失败',(object)array());
-        }
-        if($this->collectModel->add() === false){
-            json(110,'收藏失败',(object)array());
-        }
-        if($this->casesModel->where(['id'=>$cases_id])->setInc('collect_count',1) === false){
-            json(110,'收藏失败',(object)array());
-        }
-        $this->collectModel->commit();
-        json('','',(object)array());
+
     }
 
     
@@ -189,18 +215,29 @@ class IndexController extends Controller {
             switch ($cases['type']){
                 case 1:
                     $cases['is_buy'] = 1;
+                    $cases['is_collect'] = 0;
                     $cases['img'] = $this->casesImageModel->getImage($cases_id,$user_id);
                     break;
                 case 2:
                     $cases['is_buy'] = 2;
+                    $cases['is_collect'] = 0;
                     $cases['img'] = array();
             }
 
             json('','',$cases);
         }
+        $collect = $this->collectModel
+            ->where(['user_id'=>$user_id,'cases_id'=>$cases_id])
+            ->find();
+        if($collect){
+            $is_collect = 1;
+        }else{
+            $is_collect = 0;
+        }
         switch ($cases['type']){
             case 1:
                 $cases['is_buy'] = 1;
+                $cases['is_collect'] = $is_collect;
                 $cases['img'] = $this->casesImageModel->getImage($cases_id,$user_id);
 
             break;
@@ -212,19 +249,23 @@ class IndexController extends Controller {
                     ->find();
                 if($user_id = $cases['user_id']){
                     $cases['is_buy'] = 3;
+                    $cases['is_collect'] = $is_collect;
                     $cases['img'] = $this->casesImageModel->getImage($cases_id,$user_id);
                 }else{
                     if($is_buy){
                         $cases['is_buy'] = 3;
+                        $cases['is_collect'] = $is_collect;
                         $cases['img'] = $this->casesImageModel->getImage($cases_id,$user_id);
                     }else{
                         $cases['is_buy'] = 2;
+                        $cases['is_collect'] = $is_collect;
                         $cases['img'] = array();
                     }
                 }
                 break;
             default:
                 $cases['is_buy'] = 0;
+                $cases['is_collect'] = 0;
                 $cases['img'] = array();
         }
         json('','',$cases);
@@ -272,5 +313,71 @@ class IndexController extends Controller {
             ->select();
         $this->integralModel->commit();
         json('','',$images);
+    }
+
+
+    /**
+     * 获取评论列表
+     */
+    public function getCommentList()
+    {
+        $cases_id = I('post.cases_id');
+        $page = I('post.page',1);
+        $count = $this->casesCommentModel
+            ->join('user on user.id = cases_comment.user_id')
+            ->where(['cases_comment.cases_id'=>$cases_id])
+            ->count();
+        $comment_list = $this->casesCommentModel
+            ->join('user AS u on u.id = cm.user_id')
+            ->field('u.photo,u.nickname,cm.user_id,cm.content,cm.created_at')
+            ->where(['cm.cases_id'=>$cases_id])
+            ->page($page,20)
+            ->order('cm.created_at desc')
+            ->alias('cm')
+            ->select();
+        if($comment_list){
+            foreach ($comment_list as $k=>&$v){
+                $v['created_at'] = date('Y-m-d H:i:s',$v['created_at']);
+            }
+            $result = array(
+                'total_page' => ceil($count/20),
+                'list' => $comment_list
+            );
+            json('','',$result);
+        }else{
+            $result = array(
+                'total_page' => 0,
+                'list' => array()
+            );
+            json('','',$result);
+        }
+    }
+
+    /**
+     * 评论
+     */
+    public function addReply()
+    {
+        $cases_id = I('post.cases_id');
+        $user_id = I('post.user_id');
+        $this->casesCommentModel->startTrans();
+        if($this->casesCommentModel->create('','addCasesComment') === false){
+            $this->casesCommentModel->rollback();
+            json(110,'评论失败',(object)array());
+        }
+        if($this->casesCommentModel->add() === false){
+            $this->casesCommentModel->rollback();
+            json(110,'评论失败',(object)array());
+        }
+        if($this->casesModel->where(['id'=>$cases_id])->setInc('comment_count',1) === false){
+            $this->casesCommentModel->rollback();
+            json(110,'评论失败',(object)array());
+        }
+        if($this->integralModel->where(['user_id'=>$user_id])->setInc('integral',1) === false){
+            $this->casesCommentModel->rollback();
+            json(110,'评论失败',(object)array());
+        }
+        $this->casesCommentModel->commit();
+        json('','评论成功',(object)array());
     }
 }
